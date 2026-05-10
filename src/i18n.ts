6cfd3b439049040915766b962e3833586b3d5eea
mungai michael <mungaimikey@gmail.com>
2026-05-10 19:55:32 +0300

    feat: internationalize map components and migrate hub markers to data-driven layers

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslation from './locales/en.json';
+import swTranslation from './locales/sw.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      sw: {
+        translation: swTranslation,
+      },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
