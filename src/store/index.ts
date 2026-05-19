a25ee197c003e6aca0fa268747f183c421de47c5
mungai michael <mungaimikey@gmail.com>
2026-05-19 10:19:50 +0300

    feat: implement centralized state management for map, theme, and filters and using Zustand

---
+++ src/store/index.ts
@@ -0,0 +1,4 @@
+export { useMapUIStore } from "./useMapUIStore";
+export { useFilterStore } from "./useFilterStore";
+export type { FilterState, BikeMakeFilter } from "./useFilterStore";
+export { useThemeStore } from "./useThemeStore";
